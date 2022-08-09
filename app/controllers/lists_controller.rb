62e72d462968e73fbb6d4a61320e2627a1ee9e21
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-08-09 23:08:24 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,37 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to list_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    list.save
+    redirect_to list_path(list.id)
+    # showアクションにリダイレクトする場合は引数に必ずidが必要
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
